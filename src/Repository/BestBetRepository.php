019aca3b523eb53e38badb0856fd92ddd3e8e654
Ben Florin <benjamin.florin@bc.edu>
2021-02-08 16:09:56 -0500

    Update to Symfony 4.4 & Elasticsearch 7

--- src/Repository/BestBetRepository.php
+++ src/Repository/BestBetRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\BestBet;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method BestBet|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class BestBetRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, BestBet::class);
     }
--- src/Repository/BestBetTermRepository.php
+++ src/Repository/BestBetTermRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\BestBetTerm;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method BestBetTerm|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class BestBetTermRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, BestBetTerm::class);
     }
