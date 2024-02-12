c3d39673b5d724dfdc5544c7658c26bf09020f4f
Benjamin Florin <benjamin.florin@gmail.com>
2024-02-12 17:02:50 -0500

    Fix upgrade bugs

--- public/index.php
+++ public/index.php
@@ -8,11 +8,6 @@ require dirname(__DIR__).'/config/bootstrap.php';
 
 date_default_timezone_set('America/New_York');
 
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
-
-    Debug::enable();
-}
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
